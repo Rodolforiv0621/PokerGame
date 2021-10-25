7578bd3b1555c1a8b72d9d162a7ae947b5a1abf0
Rodolfo Rivera <rorivera0621@gmail.com>
2021-10-25 13:24:25 -0700

    Init commit

---
+++ src/pokergame/PokerGame.java
@@ -0,0 +1,9 @@
+package pokergame;
+public class PokerGame {
+    public static void main(String[] args) {
+        for(int i = 0;i<args.length;i++){
+            System.out.println(args[i]);
+        }
+    }
+    
+}
