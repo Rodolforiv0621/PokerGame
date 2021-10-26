cd0aae0fc6dc6395fee7d5b311a351435db13b91
Rodolfo Rivera <rorivera0621@gmail.com>
2021-10-26 15:13:42 -0700

    Test Commit

--- src/pokergame/PokerGame.java
+++ src/pokergame/PokerGame.java
@@ -1,8 +1,21 @@
 package pokergame;
 public class PokerGame {
+        static String[] communityShield = new String[5];
+        static String[] Player1;
+        static String[] Player2;
+        String[] Player3;
+        String[] Player4;
+        String[] Player5;
     public static void main(String[] args) {
+        
         for(int i = 0;i<args.length;i++){
-            System.out.println(args[i]);
+            if(i<5){
+                communityShield[i] = args[i];
+            }
+            
+        }
+        for(int j = 0;j<communityShield.length;j++){
+            System.out.println(communityShield[j].charAt(1));
         }
     }
     
